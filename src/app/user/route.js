2204f0671a42d6c85cc9866be2bb33772d60cb8f
GodSci3nc3 <rosalesvelazquezarturo@gmail.com>
2025-03-01 01:20:56 -0600

    Inserción de usuarios funcional. Hace falta vincularlo con el frontend... y crear el front

--- src/app/user/route.js
+++ src/app/user/route.js
@@ -1,6 +1,26 @@
 import {NextResponse} from 'next/server';
+import {connection} from '@/libs/mysql';
 
 
-export function POST(){
+export async function POST(request){
+try {
+    const {name, email, password, age} = await request.json();
+
+    const result = await connection.query("INSERT INTO users SET ?", 
+    {
+        name: name,
+        email: email,
+        password: password,
+        age: age
+    }
+    );
+
+    console.log(result);
+
     return NextResponse.json({message: "Creando perfil de usuario..."});
+} catch (error) {
+    console.log(error);
+    return NextResponse.json({message: "Error al crear perfil de usuario..." + error}, {status: 500});
+    
+}
 }
